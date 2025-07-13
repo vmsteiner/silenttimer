80900f8d474f1b824a21058ac1ce75c70c8c7695
vincent <vincent.steiner@gmail.com>
2025-07-13 21:34:06 +0200

    Update targetSdk version to 36

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 
 android {
     namespace = "com.github.vmsteiner.silenttimer"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.github.vmsteiner.silenttimer"
