da751842b1abc15563c120a11fc8bf9f5c9b5adc
vincent <vincent.steiner@gmail.com>
2025-07-19 15:44:45 +0200

    Increment versionCode and versionName

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.github.vmsteiner.silenttimer"
         minSdk = 33
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.1.0"
 
     }
 
